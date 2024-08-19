a24f53bb2cc3f2b2230e341ff43575bec7f7b625
Tomás Cichero <martoxdlol@gmail.com>
2024-08-19 12:09:03 -0300

    prettier config

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,16 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 const config = {
-  plugins: ["prettier-plugin-tailwindcss"],
-};
+    plugins: [
+        'prettier-plugin-tailwindcss',
+        '@trivago/prettier-plugin-sort-imports',
+    ],
+    tabWidth: 4,
+    semi: false,
+    trailingComma: 'all',
+    singleQuote: true,
+    importOrder: ['^~/server/(.*)$', '^~/trpc/(.*)$', '^~/app/(.*)$', '^[./]'],
+    importOrderSeparation: true,
+    importOrderSortSpecifiers: true,
+}
 
-export default config;
+export default config
