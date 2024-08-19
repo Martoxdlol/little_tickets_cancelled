9e4b5809bb72f0731b851bbcc629cb87aee10627
Tomás Cichero <martoxdlol@gmail.com>
2024-08-19 12:13:41 -0300

    apply prettier formats

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,11 @@
-import { type Config } from "drizzle-kit";
-
-import { env } from "~/env";
+import { type Config } from 'drizzle-kit'
+import { env } from '~/env'
 
 export default {
-  schema: "./src/server/db/schema.ts",
-  dialect: "postgresql",
-  dbCredentials: {
-    url: env.DATABASE_URL,
-  },
-  tablesFilter: ["little_tickets_*"],
-} satisfies Config;
+    schema: './src/server/db/schema.ts',
+    dialect: 'postgresql',
+    dbCredentials: {
+        url: env.DATABASE_URL,
+    },
+    tablesFilter: ['little_tickets_*'],
+} satisfies Config
