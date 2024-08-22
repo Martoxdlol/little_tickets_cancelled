a408e512b9353c6ead74a46d97fdabb59d377643
Tomás Cichero <martoxdlol@gmail.com>
2024-08-22 13:36:45 -0300

    work on basic custom editor

--- src/components/editor/index.tsx
+++ src/components/editor/index.tsx
@@ -1,89 +1,303 @@
-'use client'
-import {
-    EditorCommand,
-    EditorCommandEmpty,
-    EditorCommandItem,
-    EditorCommandList,
-    EditorContent,
-    EditorRoot,
-} from 'novel'
-import { ImageResizer, handleCommandNavigation } from 'novel/extensions'
-import { defaultExtensions } from './extensions'
-
-import { handleImageDrop, handleImagePaste } from 'novel/plugins'
+import { useLayoutEffect } from 'react'
 import { cn } from '~/lib/utils'
-import { uploadFn } from './image-upload'
-import { slashCommand, suggestionItems } from './slash-command'
 
-const extensions = [...defaultExtensions, slashCommand]
-
-const contentStylingClasses =
-    'text-sm [&_h1]:text-2xl [&_h2]:text-xl [&_h3]:text-lg [&_ul]:list-disc [&_ul]:pl-5 [&_li]:my-1 [&_p]:my-1'
-
-const Editor = () => {
+export function Editor() {
+    useLayoutEffect(() => {
+        const elem = document.getElementById('editor')
+        if (!elem) {
+            return
+        }
+        startEditor(elem as HTMLDivElement)
+    }, [])
     return (
-        <div className="relative w-full max-w-screen-lg">
-            <EditorRoot>
-                <EditorContent
-                    initialContent={{
-                        type: 'doc',
-                        content: [],
-                    }}
-                    extensions={extensions}
-                    className={cn(
-                        'min-h-[50px] border-2 border-dashed',
-                        '[&_.drag-handle]:absolute [&_.drag-handle]:size-8 [&_.drag-handle]:-translate-y-8 [&_.drag-handle]:bg-red-500',
-                        contentStylingClasses,
-                    )}
-                    editorProps={{
-                        handleDOMEvents: {
-                            keydown: (_view, event) =>
-                                handleCommandNavigation(event),
-                        },
-                        handlePaste: (view, event) =>
-                            handleImagePaste(view, event, uploadFn),
-                        handleDrop: (view, event, _slice, moved) =>
-                            handleImageDrop(view, event, moved, uploadFn),
-                        attributes: {
-                            class: 'prose prose-lg dark:prose-invert prose-headings:font-title font-default focus:outline-none max-w-full',
-                        },
-                    }}
-                    onUpdate={({ editor }) => {
-                        console.log(editor.getJSON())
-                    }}
-                    slotAfter={<ImageResizer />}
-                >
-                    <EditorCommand className="z-50 h-auto max-h-[330px] overflow-y-auto rounded-md border border-muted bg-background px-1 py-2 shadow-md transition-all">
-                        <EditorCommandEmpty className="px-2 text-muted-foreground">
-                            No results
-                        </EditorCommandEmpty>
-                        <EditorCommandList>
-                            {suggestionItems.map((item) => (
-                                <EditorCommandItem
-                                    value={item.title}
-                                    onCommand={(val) => item.command?.(val)}
-                                    className="flex w-full items-center space-x-2 rounded-md px-2 py-1 text-left text-sm hover:bg-accent aria-selected:bg-accent"
-                                    key={item.title}
-                                >
-                                    <div className="flex h-10 w-10 items-center justify-center rounded-md border border-muted bg-background">
-                                        {item.icon}
-                                    </div>
-                                    <div>
-                                        <p className="font-medium">
-                                            {item.title}
-                                        </p>
-                                        <p className="text-xs text-muted-foreground">
-                                            {item.description}
-                                        </p>
-                                    </div>
-                                </EditorCommandItem>
-                            ))}
-                        </EditorCommandList>
-                    </EditorCommand>
-                </EditorContent>
-            </EditorRoot>
+        <div
+            id="editor"
+            className={cn(
+                'min-h-[150px] w-[300px] border-2 border-dashed outline-dashed outline-1',
+                '[&_ul]:list-disc',
+                '[&_ul]:pl-5',
+            )}
+        >
+            <ul>
+                <li>
+                    <p>Asd</p>
+                </li>
+                <li>
+                    <p>asd</p>
+                </li>
+                <li>
+                    <p>
+                        adsad
+                        <br />
+                    </p>
+                </li>
+                <li>
+                    <p>Asd</p>
+                </li>
+                <li>
+                    <p>asd</p>
+                    <ul>
+                        <li>
+                            <p>Asd</p>
+                        </li>
+                        <li>
+                            <p>asd</p>
+                        </li>
+                        <li>
+                            <p>
+                                adsad
+                                <br />
+                            </p>
+                        </li>
+                        <li>
+                            <p>Asd</p>
+                        </li>
+                        <li>
+                            <p>asd</p>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
         </div>
     )
 }
 
-export default Editor
+function startEditor(element: HTMLDivElement) {
+    let lastGoodHTML = element.innerHTML
+
+    if (element.getAttribute('editor-initialized') === 'true') {
+        return
+    }
+
+    element.contentEditable = 'true'
+    element.setAttribute('editor-initialized', 'true')
+
+    let lastSelection: Selection | null = null
+
+    element.addEventListener('beforeinput', (e) => {
+        const selection = getSelection()
+
+        lastSelection = selection
+
+        if (!selection) {
+            e.preventDefault()
+        }
+
+        const node = selection?.focusNode
+        const pos = selection?.focusOffset
+
+        console.log(e.inputType)
+
+        if (!e.cancelable) {
+            return
+        }
+
+        if (e.inputType === 'deleteContentBackward') {
+            if (node?.parentNode === element) {
+                return
+            }
+
+            if (
+                node instanceof HTMLParagraphElement &&
+                pos == 0 &&
+                node.parentNode! instanceof HTMLLIElement &&
+                !node.parentNode.nextElementSibling
+            ) {
+                const li = node.parentNode
+                const ul = li.parentNode!
+
+                console.log(ul, li)
+
+                node.remove()
+                console.log(ul)
+                insertAfter(node, ul)
+                e.preventDefault()
+                setSelection(node, pos)
+
+                if (!node.parentNode.previousElementSibling) {
+                    li.remove()
+                }
+            }
+        }
+
+        if (e.inputType === 'insertParagraph') {
+            if (node instanceof HTMLParagraphElement) {
+                // alert('yey')
+                e.preventDefault()
+                console.log('APPEND NEW LINE')
+                if (
+                    node.parentElement instanceof HTMLLIElement &&
+                    !node.nextElementSibling
+                ) {
+                    const emptyListItem = createEmptyListItem()
+                    node.parentNode!.parentNode!.appendChild(emptyListItem)
+                    setSelection(emptyListItem.firstChild!, 0)
+                } else {
+                    const p = createEmptyParagraph()
+                    insertAfter(p, node)
+                    setSelection(p, 0)
+                }
+            }
+        }
+
+        validateTree(element)
+    })
+
+    element.addEventListener('input', (e) => {
+        console.log('input', e)
+
+        const isValid = validateTree(element)
+
+        if (!isValid) {
+            element.innerHTML = lastGoodHTML
+            const focusedNode = lastSelection?.focusNode
+            if (focusedNode instanceof HTMLElement) {
+                resetSelection(lastSelection)
+            }
+        } else {
+            lastGoodHTML = element.innerHTML
+        }
+    })
+}
+
+function validateTree(node: Node) {
+    if (!(node instanceof HTMLDivElement) && !(node instanceof HTMLLIElement)) {
+        console.warn('Invalid root tree node', node)
+        return false
+    }
+
+    for (const child of node.childNodes) {
+        if (!validateNode(child)) {
+            console.warn('Invalid tree child', child)
+            return false
+        }
+    }
+
+    return true
+}
+
+function validateNode(node: Node) {
+    if (node instanceof HTMLParagraphElement) {
+        return validateParagraph(node)
+    }
+
+    if (node instanceof HTMLUListElement || node instanceof HTMLOListElement) {
+        return validateListTree(node)
+    }
+
+    if (node instanceof HTMLDivElement) {
+        const p = document.createElement('p')
+        p.innerHTML = '<br>'
+        replaceNodeChild(node, p)
+        return true
+    }
+
+    return false
+}
+
+function validateParagraph(node: Node) {
+    if (!(node instanceof HTMLParagraphElement)) {
+        console.warn('Invalid paragraph', node)
+        return false
+    }
+
+    if (node.childNodes.length === 0) {
+        console.warn('Empty paragraph', node)
+        return false
+    }
+
+    return true
+}
+
+function validateListTree(node: Node) {
+    for (const child of node.childNodes) {
+        if (!validateListItemTree(child)) {
+            console.warn('Invalid list item', child)
+
+            return false
+        }
+    }
+
+    return true
+}
+
+function validateListItemTree(node: Node) {
+    if (
+        (node instanceof HTMLUListElement ||
+            node instanceof HTMLOListElement) &&
+        node.previousElementSibling instanceof HTMLLIElement
+    ) {
+        const prev = node.previousElementSibling
+        const next = node.nextElementSibling
+        node.remove()
+        prev.appendChild(node)
+
+        if (next instanceof HTMLLIElement) {
+            next.remove()
+        }
+
+        return true
+    }
+
+    if (!(node instanceof HTMLLIElement)) {
+        return false
+    }
+
+    if (node.childNodes.length === 0) {
+        return false
+    }
+
+    let isFirstChild = true
+    for (const child of node.childNodes) {
+        if (isFirstChild) {
+            if (!(child instanceof HTMLParagraphElement)) {
+                console.warn('Invalid first list item child', child)
+                return false
+            }
+            isFirstChild = false
+        }
+
+        if (!validateNode(child)) {
+            console.warn('Invalid list item child', child)
+            return false
+        }
+    }
+
+    return true
+}
+
+// Creation
+function createEmptyParagraph() {
+    const emptyParagraph = document.createElement('p')
+    emptyParagraph.innerHTML = '<br>'
+    return emptyParagraph
+}
+
+function createEmptyListItem() {
+    const emptyListItem = document.createElement('li')
+    const emptyParagraph = createEmptyParagraph()
+    emptyListItem.appendChild(emptyParagraph)
+    return emptyListItem
+}
+
+// utils
+
+function replaceNodeChild(oldChild: Node, newChild: Node) {
+    oldChild.parentNode!.replaceChild(newChild, oldChild)
+}
+
+function setSelection(node: Node, pos: number) {
+    getSelection()?.collapse(node, pos)
+}
+
+function resetSelection(selection?: Selection | null) {
+    if (!selection) {
+        return
+    }
+
+    getSelection()?.collapse(selection.focusNode, selection.focusOffset)
+}
+
+function insertAfter(node: Node, ref: Node) {
+    ref.parentNode!.insertBefore(node, ref.nextSibling)
+}
--- src/components/tickets/new-ticket-dialog.tsx
+++ src/components/tickets/new-ticket-dialog.tsx
@@ -1,8 +1,12 @@
 'use client'
 
+import dynamic from 'next/dynamic'
 import { useString } from '~/i18n/react'
-import Editor from '../editor'
 import { Dialog, DialogContent, DialogTrigger } from '../ui/dialog'
+
+const LazyEditor = dynamic(async () => (await import('../editor')).Editor, {
+    ssr: false,
+})
 
 export function NewTicketModal(props: { children: React.ReactNode }) {
     const newTicketString = useString('newTicket')
@@ -12,7 +16,8 @@ export function NewTicketModal(props: { children: React.ReactNode }) {
             <DialogTrigger asChild>{props.children}</DialogTrigger>
             <DialogContent className="flex w-full max-w-[650px] flex-col gap-2 overflow-hidden p-4">
                 <input className="bg-transparent p-0 text-lg outline-0" />
-                <Editor />
+                --
+                <LazyEditor />
             </DialogContent>
         </Dialog>
     )
